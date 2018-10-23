05433babc61d2a16c509dbf51c23f5a39aa8e63c
anildesanto <anilde_12@hotmail.com>
2018-10-23 17:46:03 +0100

    first

---
+++ precache-manifest.3a4c2b416f9d9f9dd759f3e692490b48.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5202a125d8f3fce4687b",
+    "url": "/static/js/main.5202a125.chunk.js"
+  },
+  {
+    "revision": "542d235c375a49da51b2",
+    "url": "/static/js/1.542d235c.chunk.js"
+  },
+  {
+    "revision": "5f142c634800db3af62089d72ea08622",
+    "url": "/index.html"
+  }
+];
