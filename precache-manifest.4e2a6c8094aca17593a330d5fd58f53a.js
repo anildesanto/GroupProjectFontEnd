e05b658134ca723305924f0b0f7cdb90788073dc
Mitchell Heath <42604088+Mitch-Heath@users.noreply.github.com>
2018-10-24 19:47:21 +0100

    update 2

---
+++ precache-manifest.4e2a6c8094aca17593a330d5fd58f53a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2d7ab5442f9d60a17562",
+    "url": "/static/js/main.2d7ab544.chunk.js"
+  },
+  {
+    "revision": "dc28a3af320d8dd178ba",
+    "url": "/static/js/1.dc28a3af.chunk.js"
+  },
+  {
+    "revision": "3222e6f91e4ca4d80acd02ad71bbd909",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.726a486668f1a7eebe9929127f576393.js"
+  "/precache-manifest.4e2a6c8094aca17593a330d5fd58f53a.js"
 );
 
 workbox.clientsClaim();
