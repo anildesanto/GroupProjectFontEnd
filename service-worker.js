282c466910787731e0f38de103ff40fe97df2688
Anilde Santo <anilde_12@hotmail.com>
2018-10-25 11:48:53 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5c413d7dc9a926a7c49fe85b28edeae.js"
+  "/precache-manifest.1c1919f85c4455b9dfafc68369816d5d.js"
 );
 
 workbox.clientsClaim();
